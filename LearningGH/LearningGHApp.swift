d94bcbb60984a4d64ce5de0242c265e82c4d0b27
Aadi099 <128497880+Aadi099@users.noreply.github.com>
2023-08-17 10:59:08 +0530

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Aditya Saini on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
